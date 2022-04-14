d3f47599c7e0887091c1dc6f86fdcb55126adc36
Tahoora Majlesi <tahooramajlesi@yahoo.com>
2022-04-14 16:31:45 +0430

    adding index.html and css part

---
+++ styles/index.js
@@ -0,0 +1,63 @@
+/*
+class Human {
+    gender = 'male';
+
+    printGender = () => {
+        console.log(this.gender);
+    }
+}
+
+class Person extends Human {
+    name = 'Max';
+    
+    printMyName = () => {
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+person.printMyName();
+person.printGender();
+
+const numbers = [1,2,3];
+[num1 , num2] = numbers;
+console.log(num1);
+console.log(numbers[1]);
+
+// objects and arrays are refrenced type
+const number = 1;
+const num2 = number;
+
+console.log(num2);
+
+const person = {
+    name: 'Max'
+};
+
+const secondPerson = person;
+
+person.name = "Tahoora";
+console.log(secondPerson);
+
+const thirdPerson = {
+    ...person
+};
+
+person.name = "Hadi";
+
+console.log(thirdPerson);
+
+const numbers = [1,2,3];
+
+const doubleNum = numbers.map((num) => {
+    return num*2;
+});
+
+console.log(doubleNum);
+
+const threeNum = numbers.map((num) => {
+    return num*3;
+});
+console.log(threeNum);
+*/
+
